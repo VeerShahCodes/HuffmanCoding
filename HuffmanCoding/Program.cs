0419d90f9282914517a98b3045064aba56662d06
VeerShahCodes <shahveer1213@gmail.com>
2025-06-08 14:57:10 -0700

    Add project files.

---
+++ HuffmanCoding/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HuffmanCoding
+{
+    internal class Program
+    {
+        static class HuffmanCoding
+        {
+            public static void VariableLengthEncode()
+            {
+                PriorityQueue<char, int> queue = new PriorityQueue<char, int>();
+
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
