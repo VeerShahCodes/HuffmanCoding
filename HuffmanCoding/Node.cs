008a61d5d6eb7be37ccbe2e77012129e3d2b1e26
VeerShahCodes <shahveer1213@gmail.com>
2025-06-08 15:52:29 -0700

    Errors with the decode

---
+++ HuffmanCoding/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanCoding
+{
+    public class Node
+    {
+        public int frequency;
+        public char character;
+        public Node? left;
+        public Node? right;
+        public Node(int frequency, char character)
+        {
+            this.frequency = frequency;
+            this.character = character;
+            this.left = null;
+            this.right = null;
+          
+        }
+
+        public Node(int frequency)
+        {
+            this.frequency = frequency;
+        }
+    }
+}
--- HuffmanCoding/Program.cs
+++ HuffmanCoding/Program.cs
@@ -1,18 +1,100 @@
-﻿namespace HuffmanCoding
+﻿
+namespace HuffmanCoding
 {
     internal class Program
     {
         static class HuffmanCoding
         {
-            public static void VariableLengthEncode()
+            static PriorityQueue<Node, int> queue = new PriorityQueue<Node, int>();
+            static Dictionary<char, String> codes = new Dictionary<char, String>();
+            static Dictionary<String, char> characters = new Dictionary<String, char>();
+
+            static Node root;
+            public static void VariableLengthEncode(String text)
             {
-                PriorityQueue<char, int> queue = new PriorityQueue<char, int>();
+                
+                for(int i = 0; i < text.Length; i++)
+                {
+                    int occurences = 0;
+                    for(int j = 0; j < text.Length; j++)
+                    {
+                        if(text[i] == text[j])
+                        {
+                            occurences++;
+                        }
+                    }
+                    
+                    if(occurences > 0 && !queue.UnorderedItems.Any(x => x.Element.character == text[i]))
+                    {
+                        queue.Enqueue(new Node(occurences, text[i]), occurences);
+                    }
+                }
+
+                while(queue.Count > 1)
+                {
+                    var left = queue.Dequeue();
+                    var right = queue.Dequeue();
+                    var parent = new Node(left.frequency + right.frequency);
+                    parent.left = left;
+                    parent.right = right;
+                    queue.Enqueue(parent, parent.frequency);                   
+
+                }
+                root = queue.Dequeue();
+                Traverse(root, "");
 
             }
+
+            public static String VariableLengthDecode(String code)
+            {
+                String currentCode = "";
+                String text = "";
+                for(int i = 0; i < code.Length ; i++)
+                {
+                    currentCode += code[i];
+                    if (codes.ContainsValue(currentCode))
+                    {
+                        text += characters[currentCode];
+                        currentCode = "";
+                    }
+                }
+                return text;
+            }
+
+            public static void Traverse(Node node, String code)
+            {
+                if (node.left == null && node.right == null)
+                {
+                    codes.Add(node.character, code);
+                    characters.Add(code, node.character);
+                    return;
+                }
+                if (node.left != null)
+                {
+                    Traverse(node.left, code + "0");
+                }
+                if (node.right != null)
+                {
+                    Traverse(node.right, code + "1");
+                }
+            }
+
+            public static void Write(String text)
+            {
+                for(int i = 0; i < text.Length; i++)
+                {
+                    Console.Write(codes[text[i]]);
+                }
+            }
+
+
         }
         static void Main(string[] args)
         {
-
+            HuffmanCoding.VariableLengthEncode("hello world");
+            HuffmanCoding.Write("hello world");
+            Console.WriteLine();
+            Console.WriteLine(HuffmanCoding.VariableLengthDecode("1100010111011110000101001100100010110000101100100010110000101100"));
         }
     }
 }
